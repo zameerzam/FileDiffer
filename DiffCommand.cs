7ce960c312f1b285bef0d50cc67713fd71627577
Zameer <zameer.ullah@hotmail.com>
2017-01-09 09:29:11 +0530

    Compare Files
    best tool to compare the files in Visual Studio

---
+++ DiffCommand.cs
@@ -0,0 +1,125 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="DiffCommand.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using EnvDTE;
+using System.Collections;
+using System.Collections.Generic;
+using EnvDTE80;
+using System.Linq;
+
+namespace FIleDiffer
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class DiffCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("bdef952c-c0ed-43b9-ae92-a4f44f690ce4");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DiffCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private DiffCommand(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static DiffCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new DiffCommand(package);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            var dte = (DTE2)ServiceProvider.GetService(typeof(DTE));
+            string file1, file2;
+            if(CanFilesBeCompared(dte,out file1,out file2))
+            {
+                dte.ExecuteCommand("Tools.DiffFiles", $"\"{file1}\" \"{file2}\"");
+            }
+        }
+
+        private static bool CanFilesBeCompared(DTE2 dte, out string file1, out string file2)
+        {
+            var items = GetSelectedFiles(dte);
+            file1 = items.ElementAtOrDefault(0);
+            file2 = items.ElementAtOrDefault(1);
+            return !string.IsNullOrEmpty(file1) && !string.IsNullOrEmpty(file2);
+        }
+
+        public static IEnumerable<string> GetSelectedFiles(DTE2 dte)
+        {
+            var items = (Array)dte.ToolWindows.SolutionExplorer.SelectedItems;
+            return from item in items.Cast<UIHierarchyItem>()
+                   let pi = item.Object as ProjectItem
+                   select pi.FileNames[1];
+        }
+    }
+}
